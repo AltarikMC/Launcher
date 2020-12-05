f01d522f8e8a8c585cf019d604e8135dac42e113
Quentin Legot <quentin3157.github@gmail.com>
2020-12-05 21:08:32 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const {remote} = require('electron');
+const {Menu, BrowserWindow, MenuItem, shell} = remote;
+
+const {
+    getCurrentWindow,
+    minimizeWindow,
+    unmaximizeWindow,
+    maxUnmaxWindow,
+    isWindowMaximized,
+    closeWindow,
+  } = require("./menubar.js");
+
+window.addEventListener("DOMContentLoaded", () => {
+    window.getCurrentWindow = getCurrentWindow;
+    window.minimizeWindow = minimizeWindow;
+    window.unmaximizeWindow = unmaximizeWindow;
+    window.maxUnmaxWindow = maxUnmaxWindow;
+    window.isWindowMaximized = isWindowMaximized;
+    window.closeWindow = closeWindow;
+    const minimizeButton = document.getElementById("minimize-btn");
+    const maxUnmaxButton = document.getElementById("max-unmax-btn");
+    const closeButton = document.getElementById("close-btn");
+
+    minimizeButton.addEventListener("click", e => {
+        window.minimizeWindow();
+    });
+
+    maxUnmaxButton.addEventListener("click", e => {
+        const icon = maxUnmaxButton.querySelector("#icon-maxUnmax");
+    
+        window.maxUnmaxWindow();
+    
+        // Change the middle maximize-unmaximize icons.
+        if (window.isWindowMaximized()) {
+          icon.classList.remove("icon-square");
+          icon.classList.add("icon-clone");
+        } else {
+          icon.classList.add("icon-square");
+          icon.classList.remove("icon-clone");
+        }
+      });
+
+      closeButton.addEventListener("click", e => {
+        window.closeWindow();
+      });
+});
