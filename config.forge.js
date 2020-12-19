813d9223cce757734d70a33491791267de648122
Quentin Legot <legotquentin@gmail.com>
2020-12-19 20:56:25 +0100

    Improved installation behavior + fixed font Roboto not loading

---
+++ config.forge.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+    packagerConfig: {
+        packageName: "Launcher",
+        name: "Launcher",
+        productName: "Launcher"//,
+        //icon: path.resolve(__dirname, 'app.ico')
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "Launcher",
+                iconUrl: path.resolve(__dirname, 'app.ico'),
+                //loadingGif: path.resolve(__dirname, 'src/assets/loading.gif'),
+                setupIcon: path.resolve(__dirname, 'app.ico')
+            }
+        }
+    ]
+}
