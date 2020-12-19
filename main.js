3bcac7e421c5b8722f690fd212796079f51ac595
Quentin Legot <quentin3157.github@gmail.com>
2020-12-19 20:56:25 +0100

    Improved installation behavior + fixed font Roboto not loading

--- main.js
+++ main.js
@@ -2,8 +2,60 @@ const { app, BrowserWindow, Menu, ipcMain, Notification } = require('electron')
 const path = require('path')
 const { Client, Authenticator } = require('minecraft-launcher-core')
 
+if (handleSquirrelEvent()) {
+  return;
+}
+
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const path = require('path');
+
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function(command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
+    } catch (error) {}
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function(args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      app.quit();
+      return true;
+  }
+};
+
 const launcher = new Client();
-const iconPath = path.join(__dirname, "icon.png");
+const iconPath = path.join(__dirname, "icon.ico");
 let win = null
 let auth = null
 
