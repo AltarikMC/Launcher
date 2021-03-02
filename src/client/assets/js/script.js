7bfcab4610b2fd71beffc29b3f6bc03a368d45c9
Quentin Legot <legotquentin@gmail.com>
2021-03-02 15:22:38 +0100

    removed mentions of electron.remote (still one left)

--- src/client/assets/js/script.js
+++ src/client/assets/js/script.js
@@ -1,23 +1,16 @@
 'use strict';
-const {remote, ipcRenderer} = require('electron');
+const {ipcRenderer} = require('electron');
 const {
     getCurrentWindow,
     minimizeWindow,
-    unmaximizeWindow,
-    maxUnmaxWindow,
-    isWindowMaximized,
     closeWindow,
   } = require("./assets/js/menubar.js");
 
 window.addEventListener("DOMContentLoaded", () => {
     window.getCurrentWindow = getCurrentWindow
     window.minimizeWindow = minimizeWindow
-    window.unmaximizeWindow = unmaximizeWindow
-    window.maxUnmaxWindow = maxUnmaxWindow
-    window.isWindowMaximized = isWindowMaximized
     window.closeWindow = closeWindow
     const minimizeButton = document.getElementById("minimize-btn")
-    const maxUnmaxButton = document.getElementById("max-unmax-btn")
     const closeButton = document.getElementById("close-btn")
 
     minimizeButton.addEventListener("click", e => window.minimizeWindow())
