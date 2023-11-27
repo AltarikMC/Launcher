5721b96601d3d40cfb2b20beb3027220ac678444
Quentin Legot <legotquentin@gmail.com>
2023-11-28 00:18:41 +0100

    Made a lot of server fixes thank to eslint

--- src/server/main.js
+++ src/server/main.js
@@ -1,114 +1,112 @@
 import { app, BrowserWindow, Menu, ipcMain, autoUpdater, dialog } from 'electron'
-import  logger from 'electron-log'
-import { join } from 'path'
-import updater from './updater.js'
+import isDev from 'electron-is-dev'
+import logger from 'electron-log'
+import { join, dirname } from 'path'
+import Updater from './updater.js'
 import electronStartup from 'electron-squirrel-startup'
-import install from "./install.js"
-import mc from './minecraft.js'
-import { minimizeWindow, closeWindow } from "./menubar.js";
-
+import install from './install.js'
+import Mc from './minecraft.js'
+import { minimizeWindow, closeWindow } from './menubar.js'
 import { fileURLToPath } from 'url'
-import { dirname } from 'path'
 
-
-let updaterInstance = new updater(app, autoUpdater, dialog, logger, showNotification)
+const updaterInstance = new Updater(app, autoUpdater, dialog, logger, showNotification)
 updaterInstance.configUpdater()
 
-let minecraft = new mc()
+const minecraft = new Mc()
 minecraft.showNotification = showNotification
-
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const iconPath = join(__dirname, "icon.ico")
+const iconPath = join(__dirname, 'icon.ico')
 
 let win = null
 
 function createWindow () {
-    win = new BrowserWindow({
-        width: 1000,
-        height: 600,
-        resizable: false,
-        icon: iconPath,
-        webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
-        },
-        frame: false
-    })
-    //Menu.setApplicationMenu(null)
-    win.loadFile('src/client/checkingUpdate.html')
-    win.on("close", () => {
-        app.quit()
-    })
+  win = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    resizable: false,
+    icon: iconPath,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    frame: false
+  })
+  if (!isDev) {
+    Menu.setApplicationMenu(null)
+  }
+
+  win.loadFile('src/client/checkingUpdate.html')
+  win.on('close', () => {
+    app.quit()
+  })
 }
 
-function showNotification(title, body="", clazz="info") {
-    win.webContents.send('notification', {title: title, body: body, class: clazz})
+function showNotification (title, body = '', clazz = 'info') {
+  win.webContents.send('notification', { title, body, class: clazz })
 }
 
-ipcMain.on("disconnect", () => {
-    minecraft.auth = null
-    win.loadFile('src/client/login.html').then(() =>  showNotification("Déconnecté", "Vous avez été déconnecté de votre compte", "success"))
-   
+ipcMain.on('disconnect', () => {
+  minecraft.auth = null
+  win.loadFile('src/client/login.html').then(() => showNotification('Déconnecté', 'Vous avez été déconnecté de votre compte', 'success'))
 })
 
-ipcMain.on("pageReady", (event) => {
-    event.sender.send("nick", { name: minecraft.auth.name })
-    minecraft.getModsInformations(event)
+ipcMain.on('pageReady', (event) => {
+  event.sender.send('nick', { name: minecraft.auth.name })
+  minecraft.getModsInformations(event)
 })
 
-ipcMain.on("checking-update", () => {
-    updaterInstance.checkForUpdates(win, showNotification)
+ipcMain.on('checking-update', () => {
+  updaterInstance.checkForUpdates(win, showNotification)
 })
 
-function main() {
-    if (electronStartup) {
-        install.handleSquirrelEvent(app)
-        app.quit()
-        return
+function main () {
+  if (electronStartup) {
+    install.handleSquirrelEvent(app)
+    app.quit()
+    return
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
     }
+  })
 
-    app.whenReady().then(() => {
-        createWindow()
-    })
-    
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit()
-        }
-    })
-    
-    ipcMain.on('minimizeWindow', () => {
-        minimizeWindow(win)
-    })
-    
-    ipcMain.on('closeWindow', () => {
-        closeWindow(win)
-    })
-    
-    app.on('activate', () => {
-        if (win === null){
-            createWindow()
-        }
-    })
-    
-    ipcMain.on("login", (event, args) => {
-        minecraft.login(event, win, args.user, args.pass)
-    })
-    
-    ipcMain.on("microsoft-login", (event) => {
-        minecraft.microsoftLogin(event, win)
-    })
-    
-    ipcMain.on("invalidateData", event => {
-        minecraft.invalidateData(event)
-    })
-    
-    ipcMain.on("launch", (event, args) => {
-      minecraft.launch(event, args)
-    })
+  ipcMain.on('minimizeWindow', () => {
+    minimizeWindow(win)
+  })
 
+  ipcMain.on('closeWindow', () => {
+    closeWindow(win)
+  })
+
+  app.on('activate', () => {
+    if (win === null) {
+      createWindow()
+    }
+  })
+
+  ipcMain.on('login', (event, args) => {
+    minecraft.login(event, win, args.user, args.pass)
+  })
+
+  ipcMain.on('microsoft-login', (event) => {
+    minecraft.microsoftLogin(event, win)
+  })
+
+  ipcMain.on('invalidateData', event => {
+    minecraft.invalidateData(event)
+  })
+
+  ipcMain.on('launch', (event, args) => {
+    minecraft.launch(event, args)
+  })
 }
 
 main()
--- src/server/menubar.js
+++ src/server/menubar.js
@@ -1,9 +1,9 @@
-export function minimizeWindow(browserWindow) {
-	if(browserWindow.minimizable) {
-        browserWindow.minimize()
-	}
+export function minimizeWindow (browserWindow) {
+  if (browserWindow.minimizable) {
+    browserWindow.minimize()
+  }
 }
-  
-export function closeWindow(browserWindow) {
-    browserWindow.close()
+
+export function closeWindow (browserWindow) {
+  browserWindow.close()
 }
