769120fd974bce84d84d6042862c7320c77cbec8
Quentin Legot <legotquentin@gmail.com>
2024-01-14 18:24:29 +0100

    Change discord link, Fix indent in js render files

--- src/client/assets/js/checkingUpdate.js
+++ src/client/assets/js/checkingUpdate.js
@@ -1,32 +1,32 @@
 const vue = require('vue/dist/vue.cjs.js')
 app = vue.createApp({
-    data() {
-        return {
-            displayFullscreen: "block",
-            fullscreenText: "Recherche de mise à jour...",
-            downloadLink: null
-        }
+  data () {
+    return {
+      displayFullscreen: 'block',
+      fullscreenText: 'Recherche de mise à jour...',
+      downloadLink: null
+    }
+  },
+  mounted () {
+    this.sendCheckingUpdate()
+  },
+  methods: {
+    sendCheckingUpdate () {
+      ipcRenderer.send('checking-update')
     },
-    mounted() {
-        this.sendCheckingUpdate()
-    },
-    methods: {
-        sendCheckingUpdate() {
-            ipcRenderer.send("checking-update");
-        },
-        openLinkExternal() {
-            shell.openExternal(this.downloadLink)
-        }
-    },
-});
+    openLinkExternal () {
+      shell.openExternal(this.downloadLink)
+    }
+  }
+})
 
-let root = app.mount("#vue");
+const root = app.mount('#vue')
 
-ipcRenderer.on("update-available", (event, arg) => {
-    root.fullscreenText = "Mise à jour disponible, téléchargement..."
-});
+ipcRenderer.on('update-available', (event, arg) => {
+  root.fullscreenText = 'Mise à jour disponible, téléchargement...'
+})
 
-ipcRenderer.on("please-download-update", (event, args) => {
-    root.fullscreenText = `Veuillez télécharger la mise à jour en cliquant sur le lien suivant :`
-    root.downloadLink = `${args.url}`
+ipcRenderer.on('please-download-update', (event, args) => {
+  root.fullscreenText = 'Veuillez télécharger la mise à jour en cliquant sur le lien suivant :'
+  root.downloadLink = `${args.url}`
 })
